e51dc39fa132571fe39f1c1722ba57b95edef82e
Geoff Perks <geoffrey.perks@virginholidays.com>
2016-07-18 08:09:59 +0100

    fleshing out availability aspect to the model

--- src/main/java/com/perks/emilena/api/Availability.java
+++ src/main/java/com/perks/emilena/api/Availability.java
@@ -15,18 +15,34 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 /**
+ *
+ * In a Many-to-one relationship (Persons.* - Availability.1) The availability represents a single day with the number of hours
+ * for that day. A person can have many 'availabilities'
+ *
  * Created by Geoff Perks
  * Date: 13/07/2016.
  */
 @Entity
 public class Availability implements Serializable {
 
+    /**
+     * The Date and time the availability is from
+     */
     @Column(name = "DATE_AND_TIME")
     private LocalDateTime dateAndTime;
 
+    /**
+     * A representation for the day - this can be derived from date and time.
+     */
     @Column(name = "DAY_OF_WEEK")
     @Enumerated
     private DayOfWeek dayOfWeek;
+
+    /**
+     * The number of hours for the given day that can be allocated.
+     */
+    @Column(name = "NUMBER_OF_HOURS")
+    private Integer numberOfHours;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -50,6 +66,14 @@ public class Availability implements Serializable {
 
     public void setDayOfWeek(DayOfWeek dayOfWeek) {
         this.dayOfWeek = dayOfWeek;
+    }
+
+    public Integer getNumberOfHours() {
+        return numberOfHours;
+    }
+
+    public void setNumberOfHours(Integer numberOfHours) {
+        this.numberOfHours = numberOfHours;
     }
 
     public Long getId() {
@@ -79,13 +103,14 @@ public class Availability implements Serializable {
         Availability that = (Availability) o;
         return Objects.equals(dateAndTime, that.dateAndTime) &&
                 dayOfWeek == that.dayOfWeek &&
+                Objects.equals(numberOfHours, that.numberOfHours) &&
                 Objects.equals(id, that.id) &&
                 Objects.equals(person, that.person);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(dateAndTime, dayOfWeek, id, person);
+        return Objects.hash(dateAndTime, dayOfWeek, numberOfHours, id, person);
     }
 
     @Override
@@ -93,6 +118,7 @@ public class Availability implements Serializable {
         final StringBuilder sb = new StringBuilder("Availability{");
         sb.append("dateAndTime=").append(dateAndTime);
         sb.append(", dayOfWeek=").append(dayOfWeek);
+        sb.append(", numberOfHours=").append(numberOfHours);
         sb.append(", id=").append(id);
         sb.append(", person=").append(person);
         sb.append('}');
