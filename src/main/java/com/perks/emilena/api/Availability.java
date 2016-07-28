9b7c5f237df7b2859821e109fa020b48c0c8d1ac
Geoff Perks <geoffrey.perks@virginholidays.com>
2016-07-28 17:06:19 +0100

    optimizing imports

--- src/main/java/com/perks/emilena/api/Availability.java
+++ src/main/java/com/perks/emilena/api/Availability.java
@@ -11,7 +11,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Objects;
 
--- src/main/java/com/perks/emilena/api/Person.java
+++ src/main/java/com/perks/emilena/api/Person.java
@@ -11,7 +11,6 @@ import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
